357ce774c0280265c43560c68ea7c85f1ba52942
suzhetao <suzhetao@gmail.com>
2024-12-10 11:03:32 +0800

    deploy

--- rg.tf
+++ rg.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source   = "git::ssh://github.com/goldstrike77/terraform-module-azurerm.git//resource-group?ref=v0.1"
+  source   = "git::https://github.com/goldstrike77/terraform-module-azurerm.git//resource-group?ref=v0.1"
   tags     = var.tags
   azurerm_resources = var.azurerm_resources
 }
